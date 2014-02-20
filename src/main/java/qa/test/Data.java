e120226c971aebb1b9ff6f2f2f2a1911aaf69ce0
Jon Austen <djangofan@gmail.com>
2014-02-19 22:57:02 -0800

    Minor edits.

--- src/main/java/qa/test/Data.java
+++ src/main/java/qa/test/Data.java
@@ -14,12 +14,14 @@ import org.apache.metamodel.data.Row;
 import org.apache.metamodel.excel.ExcelConfiguration;
 import org.apache.metamodel.query.SelectItem;
 import org.apache.metamodel.query.builder.InitFromBuilder;
+import org.apache.metamodel.schema.Schema;
+import org.apache.metamodel.schema.Table;
 import org.slf4j.LoggerFactory;
 
 public class Data {
-	
+
 	private static org.slf4j.Logger logger = LoggerFactory.getLogger( "Data" );
-	
+
 	public static Object[][] getExcelData( File excelFile, String sheetName ) 
 	{	
 		ExcelConfiguration conf = new ExcelConfiguration( 1, true, false );
@@ -46,33 +48,34 @@ public class Data {
 		logger.info( "Column names: " + Arrays.toString( cols ) );
 		return myArray;
 	}
-	
+
 	public static Object[][] getCsvData( File csvFile ) 
 	{	
 		CsvConfiguration conf = new CsvConfiguration( 1 );
 		DataContext csvContext = DataContextFactory.createCsvDataContext( csvFile, conf );
-		
-		
-        //csvContext.query().
-		// CsvDataSet dataSet = csvContext.query().
-		// 		.selectAll()
-		// 		.where("run").eq("Y")
-		// 		.execute();
-		// List<Row> rows = dataSet.toRows();
+		Schema schema = csvContext.getDefaultSchema();
+		Table[] tables = schema.getTables();
+		Table table = tables[0];
+		DataSet dataSet = csvContext.query()
+				.from( table )
+				.selectAll()
+				.where("run").eq("Y")
+				.execute();
+		List<Row> rows = dataSet.toRows();
 		Object[][] myArray = new Object[10][2];
-		// int i = 0;
-		// SelectItem[] cols = rows.get(0).getSelectItems();
-		// for ( Row r : rows ) {
-		// 	Object[] data = r.getValues();
-		// 	for ( int j = 0; j < cols.length; j++ ) {
-		// 		if ( data[j] == null ) data[j] = ""; // force empty string where there are NULL values
-		// 	}
-		// 	myArray[i][0] = cols;
-		// 	myArray[i][1] = data;
-		// 	i++;
-		// }
-		//logger.info( "Row count: " + rows.size() );
-		//logger.info( "Column names: " + Arrays.toString( cols ) );
+		int i = 0;
+		SelectItem[] cols = rows.get(0).getSelectItems();
+		for ( Row r : rows ) {
+			Object[] data = r.getValues();
+			for ( int j = 0; j < cols.length; j++ ) {
+				if ( data[j] == null ) data[j] = ""; // force empty string where there are NULL values
+			}
+			myArray[i][0] = cols;
+			myArray[i][1] = data;
+			i++;
+		}
+		logger.info( "Row count: " + rows.size() );
+		logger.info( "Column names: " + Arrays.toString( cols ) );
 		return myArray;
 	}
 
